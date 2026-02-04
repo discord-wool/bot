6960d6353c6525d372658fbc9026e04928da891c
discord-wool <discord_wool@proton.me>
2026-02-04 10:37:54 +0900

    Update keep_alive.py

--- src/utils/keep_alive.py
+++ src/utils/keep_alive.py
@@ -1,4 +1,3 @@
-# src/utils/keep_alive.py 
 from flask import Flask
 from threading import Thread
 
@@ -6,11 +5,15 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Renderはデフォルトでポート10000か、PORT環境変数を使用します
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True # メインプロセス終了時に一緒に終了するように設定
     t.start()
